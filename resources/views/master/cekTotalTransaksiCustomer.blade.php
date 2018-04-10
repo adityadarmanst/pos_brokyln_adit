9844d5f38ecb802eb1785affacc7f62014065dd3
Annastasya Purnama Nikita <31577022+annanikita@users.noreply.github.com>
2018-04-11 01:30:58 +0700

    Update cekTotalTransaksiCustomer.blade.php

--- resources/views/master/cekTotalTransaksiCustomer.blade.php
+++ resources/views/master/cekTotalTransaksiCustomer.blade.php
@@ -5,5 +5,5 @@ $kJumlahTransaksi = $link -> query("SELECT id FROM tbl_faktur WHERE id_costumer=
 $jlhTransaksi = mysqli_num_rows($kJumlahTransaksi);
 @endphp
 <div class="capTotalTransaksi" id="">
-{{$jlhTransaksi}}
+<b>{{$jlhTransaksi}}</b>
 </div>
